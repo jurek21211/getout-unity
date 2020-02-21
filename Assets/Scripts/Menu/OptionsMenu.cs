c8cf19baf719e8bdf216e19567ac15e597cd03bc
Jerzy Kaczmarczyk <juras1996@gmail.com>
2020-02-21 12:48:31 +0000

    Main menu with options menu functional :D

---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer masterAudioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int idx = 0; idx < resolutions.Length; idx++)
+        {
+            string option = resolutions[idx].width + "x" + resolutions[idx].height;
+            options.Add(option);
+
+            if(resolutions[idx].width == Screen.currentResolution.width &&
+                resolutions[idx].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = idx;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetVolume(float volumeValue)
+    {
+        masterAudioMixer.SetFloat("Volume", volumeValue);
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+}
